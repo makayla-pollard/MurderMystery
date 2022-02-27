efb4d9a1fe44a74752282846d857aa70c4aa528f
mpollard1 <93405109+mpollard1@users.noreply.github.com>
2022-02-27 14:33:31 -0700

    Add files via upload

---
+++ IdentityHostingStartup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MurderMystery.Data;
+
+[assembly: HostingStartup(typeof(MurderMystery.Areas.Identity.IdentityHostingStartup))]
+namespace MurderMystery.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityContext>();
+
+                services.Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequiredUniqueChars = 0;
+                    options.User.RequireUniqueEmail = false;
+
+                });
+            });
+        }
+    }
+}
